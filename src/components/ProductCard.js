83c639c4b9b52de50ec3ad475b2529fae8cbf52f
Carl Angelo Limpio <carlangelolimpio@gmail.com>
2021-12-12 16:03:05 +0800

    Added AdminUsers page

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,37 +1,29 @@
 import React from "react";
 import { Fragment } from "react";
-// import ReactImageMagnify from "react-image-magnify";
-
-// import image1 from "../ProductPhotos/product1/1.jpg";
-// import image2 from "../ProductPhotos/product1/1L.jpg";
+import ReactImageMagnify from "react-image-magnify";
 
 const ProductCard = (prop) => {
   console.log(prop.image1);
 
   return (
     <Fragment>
-      <img
-        src={`https://glacial-woodland-05160.herokuapp.com/${prop.image1}`}
-        alt="xb"
-        id="productImage"
-      />
-
-      {/*<div style = {{width: '400px'}}>
-				<ReactImageMagnify {...{
-				    smallImage: {
-				        alt: 'Wristwatch by Ted Baker London',
-				        isFluidWidth: true,
-				        src: `http://localhost:4000/${prop.image1}`
-
-
-				    },
-				    largeImage: {
-				        src: `http://localhost:4000/${prop.image2}`,
-				        width: 1600,
-				        height: 1600
-				    }
-				}} />
-			</div>*/}
+      <div style={{ width: "400px" }}>
+        <ReactImageMagnify
+          {...{
+            smallImage: {
+              alt: "Wristwatch by Ted Baker London",
+              isFluidWidth: true,
+              src: `https://glacial-woodland-05160.herokuapp.com/${prop.image1}`,
+            },
+            largeImage: {
+              src: `https://glacial-woodland-05160.herokuapp.com/${prop.image2}`,
+              width: 1600,
+              height: 1600,
+            },
+            enlargedImagePosition: "over",
+          }}
+        />
+      </div>
     </Fragment>
   );
 };
---
+++ src/pages/AdminUsers.js
@@ -0,0 +1,246 @@
+import React from "react";
+import { Fragment, useState, useEffect, useContext } from "react";
+import {
+  Table,
+  Button,
+  FloatingLabel,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import { Navigate } from "react-router-dom";
+import styled from "styled-components";
+import Swal from "sweetalert2";
+
+import UserContext from "../UserContext";
+
+const ContainerMain = styled.div`
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 30px;
+  flex-direction: column;
+`;
+const ContainerTop = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const AdminUsers = () => {
+  //To determine if admin or not
+  const { user, setUser } = useContext(UserContext);
+
+  //Set pending users list
+  const [users, setUsers] = useState([]);
+
+  //To set search parameter
+  const [search, setSearch] = useState("");
+  const [searchParam, setSearchParam] = useState("");
+
+  useEffect(() => {
+    fetch(`https://glacial-woodland-05160.herokuapp.com/users/all`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  function searchUsers(e) {
+    e.preventDefault();
+
+    fetch(`https://glacial-woodland-05160.herokuapp.com/users/specific`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        search: search,
+        searchParam: searchParam,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }
+
+  function agregateUsers(e) {
+    console.log(e);
+    if (e === 1) {
+      fetch(`https://glacial-woodland-05160.herokuapp.com/users/all`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUsers(data);
+        });
+    } else if (e === 2) {
+      fetch(`https://glacial-woodland-05160.herokuapp.com/users/admins`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUsers(data);
+        });
+    } else if (e === 3) {
+      fetch(`https://glacial-woodland-05160.herokuapp.com/users/customers`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUsers(data);
+        });
+    } else if (e === 4) {
+      fetch(
+        `https://glacial-woodland-05160.herokuapp.com/users/pendingAdmins`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setUsers(data);
+        });
+    }
+  }
+
+  function setAdminUser(x) {
+    fetch(`https://glacial-woodland-05160.herokuapp.com/users/setAdminUser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        user: x,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data === true) {
+          Swal.fire({
+            title: `You've successfully change user to admin!`,
+            icon: "success",
+            text: "Click to return to users page",
+          }).then((redirect) => {
+            window.location = "/adminUsers";
+          });
+        } else {
+          Swal.fire({
+            title: "Error",
+            icon: "error",
+            text: "Something happened.",
+          });
+        }
+      });
+  }
+
+  return user.isAdmin === false ? (
+    <Navigate to="/" />
+  ) : (
+    <Fragment>
+      <ContainerMain>
+        <ContainerTop>
+          <FloatingLabel controlId="floatingSelectGrid" label="Sort Users">
+            <Form.Select onChange={(e) => agregateUsers(e.target.value)}>
+              <option value="1">All User</option>
+              <option value="2">Admin Users</option>
+              <option value="3">Customer Users</option>
+              <option value="4">Pending Admin Users</option>
+            </Form.Select>
+          </FloatingLabel>
+          <Form className="d-flex">
+            <Form.Select
+              className="mx-3"
+              onChange={(e) => setSearchParam(e.target.value)}
+            >
+              <option>Set Search Parameter</option>
+              <option value="._id">User ID</option>
+              <option value="userEmail">User Email</option>
+            </Form.Select>
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button
+              variant="outline-success"
+              type="submit"
+              onClick={(e) => searchUsers(e)}
+            >
+              Search
+            </Button>
+          </Form>
+        </ContainerTop>
+
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>Full Name</th>
+              <th>email</th>
+              <th>Mobile Number</th>
+              <th>Type of User</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((item) => (
+              <tr>
+                <td>{item._id}</td>
+                <td>
+                  {item.firstName} {item.lastName}
+                </td>
+                <td>{item.email}</td>
+                <td>{item.mobileNo}</td>
+                <td>
+                  {item.isAdmin ? (
+                    `Admin`
+                  ) : item.joinAdmin ? (
+                    <Form.Select
+                      type="boolean"
+                      onChange={(e) => setAdminUser(item._id)}
+                    >
+                      <option value="true">Admin Request Pending</option>
+                      <option value="false">Make Admin</option>
+                    </Form.Select>
+                  ) : (
+                    `Customer`
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </ContainerMain>
+    </Fragment>
+  );
+};
+
+export default AdminUsers;
